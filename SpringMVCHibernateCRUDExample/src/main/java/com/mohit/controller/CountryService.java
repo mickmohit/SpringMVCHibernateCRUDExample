c3bc5b822cb6577e7ad4fb25e0c2e5adb7a3c6b9
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-11-09 13:08:22 +0530

    Add files via upload

---
+++ SpringMVCHibernateCRUDExample/src/main/java/com/mohit/controller/CountryService.java
@@ -0,0 +1,43 @@
+package com.mohit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mohit.dao.CountryDAO;
+import com.mohit.model.Country;
+
+@Service("countryService")
+public class CountryService {
+ 
+	@Autowired
+	CountryDAO countryDao;
+ 
+	@Transactional
+	public List getAllCountries() {
+		return countryDao.getAllCountries();
+	}
+ 
+	@Transactional
+	public Country getCountry(int id) {
+		return countryDao.getCountry(id);
+	}
+ 
+	@Transactional
+	public void addCountry(Country country) {
+		countryDao.addCountry(country);
+	}
+ 
+	@Transactional
+	public void updateCountry(Country country) {
+		countryDao.updateCountry(country);
+ 
+	}
+ 
+	@Transactional
+	public void deleteCountry(int id) {
+		countryDao.deleteCountry(id);
+	}
+}
